fbf4c34e7b8044cbf4e4c761387533030f355ff3
Ana Caroline Bariviera <ana_caroline@live.com>
2022-09-19 19:17:49 -0300

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Header from "../components/organisms/header/header"
+import Link from "next/link";
+import {appendScript} from '../utils/appendScript'
 
 export default class Login extends React.Component {
 
@@ -10,6 +11,10 @@ export default class Login extends React.Component {
     
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);        
+    }
+
+    componentDidMount () {
+        appendScript("../scripts/boxever.js");
     }
 
     handleChange(event) {
@@ -29,12 +34,12 @@ export default class Login extends React.Component {
                 <div id="contact">
                     <form id="contact-form" onSubmit={this.handleSubmit}>
                         <input type="email" name="email" placeholder="Email" id="email" value={this.state.value} onChange={this.handleChange}></input>
-                        <button type="submit" className="btn btn--primary">Login</button>
+                        <Link href="/profile">
+                            <button type="submit" className="btn btn--primary">Login</button>
+                        </Link>                    
                     </form>
                 </div>               
             </section>
         </div>);        
-    }
-    
+    }   
 }
-
