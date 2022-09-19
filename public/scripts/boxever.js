fe42ac1f9a93a6e61f5a4471f72e259b535fd3d7
Ana Caroline Bariviera <ana_caroline@live.com>
2022-09-19 19:18:48 -0300

    Add files via upload

---
+++ public/scripts/boxever.js
@@ -0,0 +1,30 @@
+var _boxeverq = _boxeverq || [];
+
+var _boxever_settings = {
+    client_key: 'pqshkqqqoklsqwue4gs09b5idg0mv1d0',
+    target: 'https://api.boxever.com/v1.2',
+    cookie_domain: '.react-test-onevaltech.vercel.app',
+    web_flow_target: "https://d35vb5cccm4xzp.cloudfront.net",
+    pointOfSale: "CapybaraHackathon"
+};
+
+(function() {
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;  
+        s.src = 'https://d1mj578wat5n4o.cloudfront.net/boxever-1.4.9.min.js';
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+})();
+
+_boxeverq.push(function() {
+    var viewEvent = {
+      channel: "WEB",
+      type: "VIEW",
+      language: "EN",
+      currency: "USD",
+      page: "login",
+      pos: "CapybaraHackathon",
+      browser_id: Boxever.getID()
+    };
+    
+    Boxever.eventCreate(viewEvent, function(data) {}, "json");
+  });
+
